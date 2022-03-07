68871175f11c1e866c293c414b62b9303f400f44
ohmree <13455401+ohmree@users.noreply.github.com>
2022-03-07 06:04:04 +0200

    Configured github pages and added site files

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV !== 'production';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
@@ -8,7 +10,16 @@ const config = {
   preprocess: preprocess(),
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs',
+    }),
+    prerender: {
+      default: true,
+    },
+    paths: {
+      base: dev ? '' : '/svelte-bootstrap-tag-input',
+    },
   },
 };
 
